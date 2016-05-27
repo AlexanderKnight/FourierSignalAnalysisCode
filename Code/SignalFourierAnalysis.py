fb424eeba602d933c8f0d4fd8b84fb375c972623
Alexander Knight <alexander.knighr@maine.edu>
2016-05-27 09:46:39 -0400

    Cleaned up graphing and added subplots

--- Code/SignalFourierAnalysis.py
+++ Code/SignalFourierAnalysis.py
@@ -43,13 +43,13 @@ abs_file_path = os.path.join(script_dir, rel_path)
 data = np.genfromtxt(abs_file_path, delimiter='\t')
 
 #Separates out values from larger array
-time = data[:,0]
-sumSignal = data[:,1]
-LR_Signal = data[:,2]
-TB_Signal = data[:,3]
+oldTime = data[:,0]
+oldSumSignal = data[:,1]
+oldLR_Signal = data[:,2]
+oldTB_Signal = data[:,3]
 
 #crops signal based on decimal fraction strength of sumSignal, if lower than specified, cropped out
-time, sumSignal, LR_Signal, TB_Signal = signalCrop(time,sumSignal,LR_Signal,TB_Signal, minSum)
+time, sumSignal, LR_Signal, TB_Signal = signalCrop(oldTime,oldSumSignal,oldLR_Signal,oldTB_Signal, minSum)
 
 #removes mean, zeroes
 sumSignal -= np.mean(sumSignal)
@@ -58,13 +58,13 @@ TB_Signal -= np.mean(TB_Signal)
 
 # does the fast fourier transform on each of the arrays and sets
 #them up to be plotted
-sumSignalF = np.fft.fft(sumSignal)
+sumSignalF = np.fft.fft(sumSignal)**2
 sumSignalF = np.abs(sumSignalF[:len(time)/2])/max(sumSignalF)
 
-LR_SignalF = np.fft.fft(LR_Signal)
+LR_SignalF = np.fft.fft(LR_Signal)**2
 LR_SignalF = np.abs(LR_SignalF[:len(time)/2])/max(LR_SignalF)
 
-TB_SignalF = np.fft.fft(TB_Signal)
+TB_SignalF = np.fft.fft(TB_Signal)**2
 TB_SignalF = np.abs(TB_SignalF[:len(time)/2])/max(TB_SignalF)
 
 #find the max value that can be plotted out to
@@ -83,36 +83,76 @@ LRPeaks, LRLows = pd.peakdet(LR_SignalF, 0.5, freq)
 TBPeaks, TBLows = pd.peakdet(TB_SignalF, 0.5, freq)
 print(sumPeaks)
 
-
+plt.figure(figsize = (20,15))
 # plots the original signals
+
+plt.subplot(3,3,1)
+plt.plot(oldTime,oldSumSignal)
+plt.title('Original Sum Signal', fontsize=16)
+plt.ylabel('Volts (V)',fontsize=12)
+plt.xlabel('Time (s)',fontsize=12)
+plt.grid()
+
+plt.subplot(3,3,2)
+plt.plot(oldTime,oldLR_Signal)
+plt.title('Original Left-Right Signal', fontsize=16)
+plt.ylabel('Volts (V)',fontsize=12)
+plt.xlabel('Time (s)',fontsize=12)
+plt.grid()
+
+plt.subplot(3,3,3)
+plt.plot(oldTime,oldTB_Signal)
+plt.title('Original Top-Bottom Signal', fontsize=16)
+plt.ylabel('Volts (V)',fontsize=12)
+plt.xlabel('Time (s)',fontsize=12)
+plt.grid()
+
+#Plots the cropped signals
+plt.subplot(3,3,4)
+plt.title('Cropped Sum Signal', fontsize=16)
+plt.ylabel('Volts (V)',fontsize=12)
+plt.xlabel('Time (s)',fontsize=12)
 plt.plot(time,sumSignal)
-plt.show()
+plt.grid()
 
+plt.subplot(3,3,5)
+plt.title('Cropped Left-Right Signal', fontsize=16)
+plt.ylabel('Volts (V)',fontsize=12)
+plt.xlabel('Time (s)',fontsize=12)
 plt.plot(time,LR_Signal)
-plt.show()
+plt.grid()
 
+plt.subplot(3,3,6)
+plt.title('Cropped Top-Bottom Signal', fontsize=16)
+plt.ylabel('Volts (V)',fontsize=12)
+plt.xlabel('Time (s)',fontsize=12)
 plt.plot(time,TB_Signal)
-plt.show()
-
-
+plt.grid()
 
 # plots  fourier transforms
+plt.subplot(3,3,7)
 plt.plot(freq,sumSignalF)
 plt.scatter(np.real(sumPeaks[:,0]), sumPeaks[:,1])
+plt.title('Fourier Transform of Cropped Sum Signal', fontsize=16)
+plt.ylabel('Volts per Hertz (V/Hz)',fontsize=12)
+plt.xlabel('Frequency (Hz)',fontsize=12)
 plt.grid()
-#plt.ylim(0,max(sumPeaks))
-#plt.xlim(0,0.2)
-plt.show()
 
+plt.subplot(3,3,8)
 plt.plot(freq,LR_SignalF)
 plt.scatter(np.real(LRPeaks[:,0]), LRPeaks[:,1])
+plt.title('Fourier Transform of Cropped Left-Right Signal', fontsize=16)
+plt.ylabel('Volts per Hertz (V/Hz)',fontsize=12)
+plt.xlabel('Frequency (Hz)',fontsize=12)
 plt.grid()
-#plt.ylim(0,max(LRPeaks))
-plt.show()
 
+plt.subplot(3,3,9)
 plt.plot(freq,TB_SignalF)
 plt.scatter(np.real(TBPeaks[:,0]),TBPeaks[:,1])
+plt.title('Fourier Transform of Cropped Top-Bottom Signal', fontsize=16)
+plt.ylabel('Volts per Hertz (V/Hz)',fontsize=12)
+plt.xlabel('Frequency (Hz)',fontsize=12)
 plt.grid()
-#plt.ylim(0,max(TBPeaks)/10)
-#plt.xlim(0,8)
+
+plt.tight_layout()
 plt.show()
